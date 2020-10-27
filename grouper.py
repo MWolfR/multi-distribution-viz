5c552674ff78229c9c7f9fb3659ec3d97429591e
Michael Reimann <michael@reimann.science>
2020-10-27 09:26:37 +0100

    Added option for direct comparison with half-violins

--- grouper.py
+++ grouper.py
@@ -28,21 +28,39 @@ class DistributionGrouper(object):
         return [(val, numpy.hstack(filtered.get(**dict([(category, val)]))))
                 for val in filtered.labels_of(category)]
 
-    def violins_for(self, parameter, lst_filter_values, main_category):
+    def violins_for(self, parameter, lst_filter_values, main_category, comparator=None):
         fltr_dict = dict([(category, values)
                           for category, values in zip(self._filter_categories,
                                                       lst_filter_values)
                           if len(values)])
-        values = self.values_across(parameter, main_category, **fltr_dict)
-        return [go.Violin(x0=k, y=v, name=k) for k, v in values]
+        if comparator is not None:
+            fltr_dict[comparator[0]] = comparator[1][0]
+            values = self.values_across(parameter, main_category, **fltr_dict)
+            violins = [go.Violin(x0=k, y=v, name=comparator[1][0],
+                                 side='negative', line={"color": 'blue'},
+                                 scalegroup=k, showlegend=(k == values[0][0]),
+                                 meanline_visible=True, legendgroup=comparator[1][0])
+                       for k, v in values]
+            fltr_dict[comparator[0]] = comparator[1][-1]
+            values = self.values_across(parameter, main_category, **fltr_dict)
+            violins = violins + [go.Violin(x0=k, y=v, name=comparator[1][-1],
+                                           side='positive', line={"color": "orange"},
+                                           scalegroup=k, showlegend=(k == values[0][0]),
+                                           meanline_visible=True, legendgroup=comparator[1][-1])
+                                 for k, v in values]
+        else:
+            values = self.values_across(parameter, main_category, **fltr_dict)
+            violins = [go.Violin(x0=k, y=v, name=k, meanline_visible=True) for k, v in values]
+        return violins
 
     def make_empty(self):
         fwgt = go.FigureWidget(data=[])
         fwgt.layout.height = self._height
         return fwgt
 
-    def make_figure(self, parameter, lst_filter_values, main_category):
-        fwgt = go.FigureWidget(data=self.violins_for(parameter, lst_filter_values, main_category),
+    def make_figure(self, parameter, lst_filter_values, main_category, comparator=None):
+        fwgt = go.FigureWidget(data=self.violins_for(parameter, lst_filter_values, main_category,
+                                                     comparator=comparator),
                                layout=go.Layout())
         fwgt.layout.height = self._height
         return fwgt
--- viz-multi-distribution.py
+++ viz-multi-distribution.py
@@ -17,9 +17,27 @@ def read_dfs(lst_dfs):
 def make_parameter_dropdown(grp_obj):
     opt_dict = [dict([('label', v), ('value', v)]) for v in grp_obj._parameters]
     return dcc.Dropdown(
-            options=opt_dict,
-            value=grp_obj._column_used_for_lookup,
-            id="parameter-dropdown")
+        options=opt_dict,
+        value=grp_obj._column_used_for_lookup,
+        id="parameter-dropdown",
+        clearable=False)
+
+
+def make_comparator_dropdown(grp_obj):
+    category_opt_dict = [dict([('label', v), ('value', v)]) for v in grp_obj._filter_categories]
+    value_opt_dict = []
+    comp_category_dd = dcc.Dropdown(
+        options=category_opt_dict,
+        value=None,
+        id="comparator-category-dropdown"
+    )
+    comp_value_dd = dcc.Dropdown(
+        options=value_opt_dict,
+        value=None,
+        id="comparator-value-dropdown",
+        multi=True
+    )
+    return comp_category_dd, comp_value_dd
 
 
 def make_filter_dropdowns(grp_obj):
@@ -41,17 +59,21 @@ def make_grouper_dropdown(grp_obj):
     return dcc.Dropdown(
         options=opt_dict,
         value="target",  # TODO: not hard coded
-        id="grouper-dropdown")
+        id="grouper-dropdown",
+        clearable=False
+    )
 
 
-def make_layout(parameter_dropdown, filter_dropdowns, grouper_dropdown):
-    col_width = 400  # TODO: not hard coded
+def make_layout(parameter_dropdown, filter_dropdowns, grouper_dropdown, comp_cat_dropdown, comp_val_dropdown):
+    col_width = 200  # TODO: not hard coded
     lo_rows = [html.Tr([html.Th("Parameter", scope="col", style={"width": col_width}),
-                        html.Th("Group by", scope="col", style={"width": col_width})
+                        html.Th("Group by", scope="col", style={"width": col_width}),
+                        html.Th("Compare category", scope="col", style={"width": col_width}),
+                        html.Th("Compare values", scope="col", style={"width": col_width})
                         ]),
-               html.Tr([html.Td(parameter_dropdown), html.Td(grouper_dropdown)]),
-               html.Tr([html.Td(html.Div([html.Label(_dd.id), _dd])) for _dd in filter_dropdowns[:2]]),
-               html.Tr([html.Td(html.Div([html.Label(_dd.id), _dd])) for _dd in filter_dropdowns[2:]])  # TODO: not hard coded
+               html.Tr([html.Td(parameter_dropdown), html.Td(grouper_dropdown),
+                        html.Td(comp_cat_dropdown), html.Td(comp_val_dropdown)]),
+               html.Tr([html.Td(html.Div([html.Label(_dd.id), _dd])) for _dd in filter_dropdowns])
                ]
     return html.Table(lo_rows)
 
@@ -64,14 +86,17 @@ def main(lst_dbs, return_app=False):
     parameter_dropdown = make_parameter_dropdown(grp_obj)
     filter_dropdowns = make_filter_dropdowns(grp_obj)
     grouper_dropdown = make_grouper_dropdown(grp_obj)
+    comp_cat_dd, comp_val_dd = make_comparator_dropdown(grp_obj)
     fig = grp_obj.make_empty()
 
-    inputs = [Input(parameter_dropdown.id, 'value'), Input(grouper_dropdown.id, 'value')] +\
+    inputs = [Input(parameter_dropdown.id, 'value'), Input(grouper_dropdown.id, 'value'),
+              Input(comp_cat_dd.id, 'value'), Input(comp_val_dd.id, 'value')] +\
         [Input(_fltr_cat, 'value') for _fltr_cat in grp_obj._filter_categories]
 
     app = dash.Dash(__name__)
 
-    controls_layout = make_layout(parameter_dropdown, filter_dropdowns, grouper_dropdown)
+    controls_layout = make_layout(parameter_dropdown, filter_dropdowns, grouper_dropdown,
+                                  comp_cat_dd, comp_val_dd)
     app.layout = html.Div([
         controls_layout,
         dcc.Graph(id='main-graph', figure=fig)
@@ -82,8 +107,21 @@ def main(lst_dbs, return_app=False):
         Output('main-graph', 'figure'),
         inputs
     )
-    def master_callback(parameter, group_by, *args):
+    def master_callback(parameter, group_by, comparator_category, comparator_value, *args):
+        if comparator_value is not None and comparator_category is not None:
+            if len(comparator_value):
+                return grp_obj.make_figure(parameter, args, group_by,
+                                           comparator=(comparator_category, comparator_value))
         return grp_obj.make_figure(parameter, args, group_by)
+
+    @app.callback(
+        Output(comp_val_dd.id, 'options'),
+        Input(comp_cat_dd.id, 'value')
+    )
+    def comparator_callback(category_selected):
+        if category_selected is None:
+            return []
+        return [dict([('label', v), ('value', v)]) for v in grp_obj._filter_possible_values[category_selected]]
 
     if return_app:
         return app
